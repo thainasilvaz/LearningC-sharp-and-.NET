e6a8dab28b041acab6850242d6fe04ae6541a231
Thainá Ferreira da Silva <thaina.ferreira026@gmail.com>
2026-02-16 11:30:37 -0300

    consolidando conhecimentos

--- Models/Estacionamento.cs
+++ Models/Estacionamento.cs
@@ -14,7 +14,6 @@ namespace DesafioFundamentos.Models
 
         public void AdicionarVeiculo()
         {
-            // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
 
             Console.WriteLine("Digite a placa do veículo para estacionar: ");
             string placa = Console.ReadLine();
@@ -41,14 +40,7 @@ namespace DesafioFundamentos.Models
                 horas = int.Parse(Console.ReadLine());
 
                 valorTotal = precoInicial + precoPorHora * horas;
-
-                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
-                // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
-                // *IMPLEMENTE AQUI*
-
-                // TODO: Remover a placa digitada da lista de veículos
-                // *IMPLEMENTE AQUI*
-
+                
                 veiculos.Remove(placa);
                 Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}.");
             }
@@ -68,8 +60,6 @@ namespace DesafioFundamentos.Models
                 {
                     Console.WriteLine($"{veiculos[i]}");
                 }
-                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
-                // *IMPLEMENTE AQUI*
             }
             else
             {
