cf2b47c08f245810f4781136ee10dab8b235c906
thainasilvaz <thaina.ferreira026@gmail.com>
2026-02-16 10:54:26 -0300

    arrays e listas

--- Program.cs
+++ Program.cs
@@ -1,107 +1,27 @@
-﻿using fundamentos_.NET_e_C_.Models;
+﻿//arrys e listas
 
-Calculadora calc = new Calculadora(); //instanciando a classe Calculadora -> para utilizar a classe é necessário criar uma variável do tipo da classe (nesse caso, do tipo Calculadora)
+List<string> listaString = new List<string>();
 
-/*
-calc.Soma(2,2);
-calc.Subtracao(2,2);
-calc.Divisao(2,2);
-calc.Multiplicacao(2,2);
-calc.Potenciacao(2,2);
-calc.Seno(30);
-calc.Cosseno(30);
-calc.Tangente(30);
-calc.RaizQuadrada(25);
-*/
+listaString.Add("SP");
+listaString.Add("BA");
+listaString.Add("MG");
 
-//Teste menu interativo
-
-string opcao;
-
-while (true)
+for (int i = 0; i < listaString.Count; i++)
 {
-    Console.WriteLine("Digite sua opção: ");
-    Console.WriteLine("1 - Cadastrar cliente");
-    Console.WriteLine("2 - Buscar cliente");
-    Console.WriteLine("3 - Apagar cliente");
-    Console.WriteLine("4 - Encerrar programa \n");
-
-    opcao = Console.ReadLine();
-
-    switch (opcao)
-    {
-        case "1":
-            Console.WriteLine("Cadastrando cliente... \n");
-            break;
-        case "2":
-            Console.WriteLine("Buscando cliente... \n");
-            break;
-        case "3":
-            Console.WriteLine("Apagando cliente... \n");
-            break;
-        case "4":
-            Console.WriteLine("Encerrando programa...");
-            Environment.Exit(0);
-            break;
-        default:
-            Console.WriteLine("Opção Inválida. \n");
-            break;
-    }
+    Console.WriteLine($"Posição nº {i} - {listaString[i]}");
 }
 
+/*int[] arrayInteiros = new int[3];
 
+arrayInteiros[0] = 10;
+arrayInteiros[1] = 20;
+arrayInteiros[2] = 30;
 
+for (int i = 0; i < arrayInteiros.Length; i++)
+{
+    Console.WriteLine($"Posição nº {i} - {arrayInteiros[i]}");
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// para comentar um bloco de codigo com linhas individuais: ctrl + K + C com o bloco selecionado
-// para comentar um bloco de código de uma só vez: alt + shift + A
-
-//DateTime dataAtual = DateTime.Now;
-
-//Console.WriteLine(dataAtual);
+int[] arrayInteirosDobrado = new int[arrayInteiros.Length * 2]; //array com o dobro do tamanho do outro
+Array.Copy(arrayInteiros, arrayInteirosDobrado, arrayInteiros.Length); //copiando os elementos do arrayInteiros para o arrayInteirosDobrado
+*/
